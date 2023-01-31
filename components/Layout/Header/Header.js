2733b0fdbf0d78a443c5bd3cfd452ec2011af50a
Loic Verrall <loic.verrall@gmail.com>
2023-01-31 17:15:11 -0300

    Add home page link to name text in header

--- components/Layout/Header/Header.js
+++ components/Layout/Header/Header.js
@@ -1,9 +1,10 @@
-import styles from './styles.module.css';
+import Link from "next/link";
+import styles from "./styles.module.css";
 
 const Header = () => (
-	<header className={styles.header}>
-		LOIC VERRΛLL
-	</header>
+  <header className={styles.header}>
+    <Link href="/">LOIC VERRΛLL</Link>
+  </header>
 );
 
 export default Header;
