196fda020375ac504be6ee4ac1aeb1ebd25679a1
Loic Verrall <loic@loic.tech>
2023-08-13 17:46:29 +0100

    Remove unused Header component

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,10 +1,8 @@
 import styles from "./styles.module.css";
-import Header from "./Header";
 import Footer from "./Footer";
 
 export const Layout = ({ children }) => (
   <div className={styles.container}>
-    <Header />
     {children}
     <Footer />
   </div>
