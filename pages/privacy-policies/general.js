f968922f74d6ecd185153755b758c95de2fb86f4
Loic Verrall <loic@loic.tech>
2023-08-13 18:44:43 +0100

    Add general privacy policy page

---
+++ pages/privacy-policies/general.js
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+
+export default function GeneralPrivacyPolicy() {
+  return (
+    <div style={{ color: 'white', padding: '4em', textAlign: 'center' }}>
+      <Head>
+        <title>
+          Privacy Policy
+        </title>
+      </Head>
+
+      <p>
+        We do not store any of your data.
+      </p>
+    </div>
+  )
+};
